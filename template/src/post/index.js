da181f4b60d3d4f96e605fb73421e87efcad99ce
meetcw <meetcw@outlook.com>
2021-03-11 23:56:47 +0800

    .

--- template/src/post/index.js
+++ template/src/post/index.js
@@ -4,6 +4,6 @@ import Header from '../share/header'
 import '../share/footer'
 
 Header.init()
-document.querySelector('.post-wrapper .side .toggle').onclick = function () {
-    document.querySelector('.post-wrapper').classList.toggle('with-side')
+document.querySelector('.slide-wrapper .toggle').onclick = function () {
+    document.body.classList.toggle('show-slide')
 }
